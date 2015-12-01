a5474278bc182abf553642139592a3f5b33acbe1
Ryan Maxwell <home@ryans-macbook-pro.local>
2015-12-01 11:04:05 -0500

    Initial Commit

---
+++ Eye-Genie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eye-Genie
+//
+//  Created by Ryan Maxwell on 2015-12-01.
+//  Copyright © 2015 Bceen Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
